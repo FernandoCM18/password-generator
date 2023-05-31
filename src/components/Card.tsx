7fe45a573da31f68e2b236764a2871a17c012bed
FernandoCM18 <fernando.c.m@live.com>
2023-05-31 15:58:22 -0700

    feat: ✨ Add conditions and const
    Add variables to show a message in Label and conditions to show the security level

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,18 +8,22 @@ import { evaluatePassword } from '../helpers/evaluatePassword';
 
 export const Card = () => {
   let message = '';
+  const INFO_TEXT = 'Genera una contraseña';
   const password = useStore((state) => state.password);
   const activeCount = evaluatePassword(password);
-  if (activeCount === 0) message = 'Muy débil';
-  if (activeCount === 1) message = 'Débil';
-  if (activeCount === 2) message = 'Buena';
-  if (activeCount === 3) message = 'Fuerte';
-  if (activeCount === 4) message = 'Muy fuerte';
+
+  if (password.length === 0) message = '';
+  if (activeCount === 0 && password.length > 0) message = 'Muy débil';
+  if (activeCount === 1 && password.length > 0) message = 'Débil';
+  if (activeCount === 2 && password.length > 0) message = 'Buena';
+  if (activeCount === 3 && password.length > 0) message = 'Fuerte';
+  if (activeCount === 4 && password.length > 0) message = 'Muy fuerte';
+
   
   return (
-    <article className="bg-white p-5 rounded-xl space-y-4 shadow-xl">
+    <article className="bg-white p-5 rounded-xl space-y-4 shadow-xl max-w-[327px]">
       <h2 className="text-2xl font-bold">Generador de contraseña</h2>
-      <Label />
+      <Label text={password || INFO_TEXT} />
       <ProgressBar />
       <Options />
       <SecurityLevel activeCount={activeCount} message={message} />
