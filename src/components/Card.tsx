8e1d746286679470dcb482c309d0d40c0add64f3
FernandoCM18 <fernando.c.m@live.com>
2023-05-30 21:53:41 -0700

    fix: 💄 change in LabelPassword component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,20 +5,15 @@ import { ProgressBar } from './ProgressBar';
 import { SecurityLevel } from './SecurityLevel';
 
 export const Card = () => {
-
-
-
-  
   return (
     <article className="bg-white p-5 rounded-xl space-y-4 shadow-xl">
       <h2 className="text-2xl font-bold">Generador de contraseña</h2>
-      <LabelPassword label="Password" />
+      <LabelPassword />
       <ProgressBar />
       <Options />
       <SecurityLevel activeCount={3} />
       <Button 
         name="Generar contraseña" 
-        onClick={() => {console.log('click');}} 
       />
     </article>
   );
