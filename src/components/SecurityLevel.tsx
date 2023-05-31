580589ce4def1d26257e8337a272fef90b8d8482
FernandoCM18 <fernando.c.m@live.com>
2023-05-31 15:11:37 -0700

    feat: ✨ Add new prop message
    Add a new prop message to show messages about how strong the password

--- src/components/SecurityLevel.tsx
+++ src/components/SecurityLevel.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 interface Props {
   activeCount: number;
+  message?: string;
 }
 
-export const SecurityLevel = ({activeCount}: Props) => {
+export const SecurityLevel = ({activeCount, message}: Props) => {
   const [totalBars] = useState(4);
 
   const activeColor = 'text-gray-500';
@@ -23,7 +24,7 @@ export const SecurityLevel = ({activeCount}: Props) => {
 
   return (
     <div className="flex justify-between items-center bg-gray-100 p-4">
-      <p className="text-gray-500">Nivel de seguridad</p>
+      <p className="text-gray-500">Nivel de seguridad: <span className="font-bold">{message}</span></p>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-9 h-9">
         {bars}
       </svg>
