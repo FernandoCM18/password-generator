9a26f565e0e8cca4aaa42eed763d7d7786edbe1f
FernandoCM18 <fernando.c.m@live.com>
2023-05-30 21:48:50 -0700

    feat: ✨ add global values and generate a password

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,37 @@
+import { shallow } from 'zustand/shallow';
+import { generatePassword } from '../helpers/generatePassword';
+import { useStore } from '../store/store';
+
 interface Props {
   name: string;
-  onClick?: () => void;
 }
-export const Button = ({name, onClick}: Props) => {
+export const Button = ({name}: Props) => {
+  const values = useStore((state) => ({
+    lengthPassword: state.lengthPassword,
+    uppercase: state.uppercase,
+    lowercase: state.lowercase,
+    numbers: state.numbers,
+    symbols: state.symbols,
+    setPassword: state.setPassword,
+  }), shallow);
+  
+  const handleClick = () => {
+    const password = generatePassword({
+      length: values.lengthPassword,
+      uppercase: values.uppercase,
+      lowercase: values.lowercase,
+      numbers: values.numbers,
+      symbols: values.symbols,
+    });
+    values.setPassword(password);
+  };
+
+
   return (
     <button 
-      className="bg-blue-700 text-white font-bold w-full p-4"
-      onClick={onClick}
+      className="bg-blue-700 text-white font-bold w-full p-4 disabled:bg-gray-200 disabled:text-gray-500 disabled:cursor-not-allowed"
+      onClick={handleClick}
+      disabled={values.lengthPassword === 0 || values.uppercase === false && values.lowercase === false && values.numbers === false && values.symbols === false }
     >
       {name}
     </button>
