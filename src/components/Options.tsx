bf3bbe94203a77b9c385848092a9fcd571f8a6f5
FernandoCM18 <fernando.c.m@live.com>
2023-05-30 21:55:19 -0700

    feat: ✨ add global values and checkbox functionality

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,3 +1,5 @@
+import { shallow } from 'zustand/shallow';
+import { useStore } from '../store/store';
 
 const OPTIONS = [
   {
@@ -18,8 +20,16 @@ const OPTIONS = [
   }
 ];
 export const Options = () => {
-  
-
+  const {uppercase, lowercase, numbers, symbols, changeUppercase, changeLowercase, changeNumbers, changeSymbols} = useStore((state) => ({
+    uppercase: state.uppercase,
+    lowercase: state.lowercase,
+    numbers: state.numbers,
+    symbols: state.symbols,
+    changeUppercase: state.changeUppercase,
+    changeLowercase: state.changeLowercase,
+    changeNumbers: state.changeNumbers,
+    changeSymbols: state.changeSymbols,
+  }), shallow);
 
   return (
     <div className="flex flex-col">
@@ -29,7 +39,9 @@ export const Options = () => {
             <input 
               className="form-checkbox rounded text-blue-700 border-2 mr-2 focus:ring-0" 
               type="checkbox" 
-              name={option.name} 
+              name={option.name}
+              checked={option.name === 'uppercase' ? uppercase : option.name === 'lowercase' ? lowercase : option.name === 'numbers' ? numbers : symbols}
+              onChange={option.name === 'uppercase' ? changeUppercase : option.name === 'lowercase' ? changeLowercase : option.name === 'numbers' ? changeNumbers : changeSymbols}
               id={option.name}
             />
             {option.label}
