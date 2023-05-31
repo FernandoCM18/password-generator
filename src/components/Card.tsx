667e099a37a4c53c0b6c3234098d21a0345dda54
FernandoCM18 <fernando.c.m@live.com>
2023-05-31 15:07:04 -0700

    feat: ✨ Add function and useStore
    Add a function to evaluate passwords and useStore to bring password

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,28 @@
+import { useStore } from '../store/store';
 import { Button } from './Button';
 import { Label } from './Label';
 import { Options } from './Options';
 import { ProgressBar } from './ProgressBar';
 import { SecurityLevel } from './SecurityLevel';
+import { evaluatePassword } from '../helpers/evaluatePassword';
 
 export const Card = () => {
+  let message = '';
+  const password = useStore((state) => state.password);
+  const activeCount = evaluatePassword(password);
+  if (activeCount === 0) message = 'Muy débil';
+  if (activeCount === 1) message = 'Débil';
+  if (activeCount === 2) message = 'Buena';
+  if (activeCount === 3) message = 'Fuerte';
+  if (activeCount === 4) message = 'Muy fuerte';
+  
   return (
     <article className="bg-white p-5 rounded-xl space-y-4 shadow-xl">
       <h2 className="text-2xl font-bold">Generador de contraseña</h2>
       <Label />
       <ProgressBar />
       <Options />
-      <SecurityLevel activeCount={3} />
+      <SecurityLevel activeCount={activeCount} message={message} />
       <Button 
         name="Generar contraseña" 
       />
