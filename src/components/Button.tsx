55de7132a94ee6aa626f1d4356c35bb4be9c5a51
FernandoCM18 <fernando.c.m@live.com>
2023-06-03 14:24:22 -0700

    fix: 💄 Add hover and roudend

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,7 +20,7 @@ export const Button = ({name}: Props) => {
 
   return (
     <button 
-      className="bg-blue-700 text-white font-bold w-full p-4 disabled:bg-gray-200 dark:disabled:bg-slate-800 disabled:text-gray-500 dark:disabled:text-slate-600 dark:disabled:opacity-60 disabled:cursor-not-allowed"
+      className="bg-blue-600 text-white font-bold w-full p-4 rounded-lg hover:bg-blue-500 disabled:bg-gray-200 dark:disabled:bg-slate-800 disabled:text-gray-500 dark:disabled:text-slate-600 dark:disabled:opacity-60 disabled:cursor-not-allowed"
       onClick={handleClick}
       disabled={lengthPassword === 0 || uppercase === false && lowercase === false && numbers === false && symbols === false }
     >
