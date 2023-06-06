8ac770cf6529ddfdc7d0c0bd15585c8f0a4e9d99
FernandoCM18 <fernando.c.m@live.com>
2023-06-06 15:35:22 -0700

    fix: ♻️ Delete props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react';
-import { useTheme } from './hooks/useTheme';
 import { Card, Container, Toast, ToggleTheme} from './components/index';
 
 function App() {
   const [showToast, setShowToast] = useState(false);
-  const {theme, toggleTheme } = useTheme();
   
   return (
     <div className="bg-slate-50 dark:bg-slate-950">
       <Container>
-        <ToggleTheme theme={theme} toggleTheme={toggleTheme} />
+        <ToggleTheme />
         {showToast && <Toast message="¡Contraseña copiada!" />}
         <Card 
           title="Generador de contraseña"
