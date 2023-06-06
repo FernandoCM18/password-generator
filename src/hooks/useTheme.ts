3fa2d05de3f1c2e159a94d2a6e8059d548fe99c0
FernandoCM18 <fernando.c.m@live.com>
2023-06-06 15:40:18 -0700

    feat: ✨ hook for controlate theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,17 +1,48 @@
 import { useEffect, useState } from 'react';
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [theme, setTheme] = useState('system');
+  const element = document.body;
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');  
 
   useEffect(() => {
-    const body = document.querySelector('body');
-    body?.classList.remove('light', 'dark');
-    body?.classList.add(theme);
-    localStorage.setItem('theme', theme);
-  }, [theme]);
+    if (!(localStorage.getItem('theme'))) {
+      const handleThemeChange = (e: MediaQueryListEvent) => {
+        const newTheme = e.matches ? 'dark' : 'light';
+        if (newTheme === 'dark') {
+          element.classList.remove('light');
+          element.classList.add('dark');
+        } else {
+          element.classList.remove('dark');
+          element.classList.add('light');
+        }
+      };
+  
+      mql.addEventListener('change', handleThemeChange);
+  
+      return () => {
+        mql.removeEventListener('change', handleThemeChange);
+      };
+    }
 
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-  return { theme, toggleTheme};
+  }, [mql, element]);
+
+  switch (theme) {
+  case 'light':
+    element.classList.remove('dark');
+    element.classList.add('light');
+    localStorage.setItem('theme', 'light');
+    break;
+  case 'dark':
+    element.classList.remove('light');
+    element.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+    break;
+  default:
+    localStorage.removeItem('theme');
+    mql.matches ? element.classList.add('dark') : element.classList.remove('dark');
+    break;
+  }
+
+  return { theme, setTheme };
 };
